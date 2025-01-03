69395d858d42604452d4b1e1c42781503e829604
Coin Hunters <111747226+CoinHuntersTR@users.noreply.github.com>
2025-01-03 11:49:33 +0300

    Update DefaultLayout.vue

--- src/layouts/components/DefaultLayout.vue
+++ src/layouts/components/DefaultLayout.vue
@@ -265,7 +265,7 @@ function selected(route: any, nav: NavLink) {
           class="py-2 px-4 flex items-center cursor-pointer rounded-lg hover:bg-gray-100 dark:hover:bg-[#373f59]"
         >
           <img
-            src="https://pbs.twimg.com/profile_images/1818315886618615808/h0wR_0La_400x400.jpg"
+            src="https://pbs.twimg.com/profile_images/1854933222569975808/no3lt9ZL_400x400.jpg"
             class="w-6 h-6 rounded-full mr-3"
           />
           <div
